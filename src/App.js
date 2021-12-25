c9a78ecfab57bf864399dafeec76bbc7fde28187
Lucas Pintos <lucaspintos909@gmail.com>
2021-12-25 13:49:03 -0300

    feat: Haciendo fetch de gifs y actualizando estado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
+const apiURL =
+  "https://api.giphy.com/v1/gifs/search?api_key=ilH61ZakLMNx7CdP457gQ4DLRn7ihoOR&q=pandas&limit=25&offset=0&rating=g&lang=en";
+
 function App() {
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(() => {
+    fetch(apiURL)
+      .then((res) => res.json())
+      .then((response) => {
+        const { data } = response;
+        const resGifs = data.map((image) => image.images.downsized_medium.url);
+        setGifs(resGifs);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <section className="app__content"></section>
+      <section className="app__content">
+        {gifs.map((gif) => (
+          <img src={gif} />
+        ))}
+      </section>
     </div>
   );
 }
