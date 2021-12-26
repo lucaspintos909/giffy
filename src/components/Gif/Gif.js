aa8d876e93a8854e17ce0e6e3fe136d5d442849e
Lucas Pintos <lucaspintos909@gmail.com>
2021-12-26 02:34:01 -0300

    refactor: Convirtiendo el gif en un a con id asociado

--- src/components/Gif/Gif.js
+++ src/components/Gif/Gif.js
@@ -3,10 +3,10 @@ import "./Gif.css";
 
 export default function Gif({ id, title, url }) {
   return (
-    <div className="gif">
+    <a href={`#${id}`} className="gif">
       <img className="gif__img" src={url} alt={title} />
       <p className="gif__title">{title}</p>
       <small className="gif__id">{id}</small>
-    </div>
+    </a>
   );
 }
