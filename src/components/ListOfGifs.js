5035ae2c1c105a990ec118518ad68d79c990d05b
Lucas Pintos <lucaspintos909@gmail.com>
2021-12-26 02:31:49 -0300

    refactor: Utilizando parametros que da wouter

--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -2,16 +2,22 @@ import React, { useEffect, useState } from "react";
 import Gif from "./Gif/Gif";
 import getGifs from "../services/getGifs";
 
-export default function ListOfGifs({ keyword }) {
+export default function ListOfGifs({ params }) {
+  const { keyword } = params;
+
   const [gifs, setGifs] = useState([]);
 
   useEffect(() => {
-    const gifs = getGifs({ keyword }).then((gifs) => {
-      setGifs(gifs);
+    getGifs({ keyword }).then((newGifs) => {
+      setGifs(newGifs);
     });
   }, [keyword]);
 
-  return gifs.map(({ id, title, url }) => (
-    <Gif id={id} title={title} url={url} key={id} />
-  ));
+  return (
+    <>
+      {gifs.map(({ id, title, url }) => (
+        <Gif id={id} title={title} url={url} key={id} />
+      ))}
+    </>
+  );
 }
