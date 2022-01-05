7cffdb8fa7f3e0d4131a83f150c5715f6a628461
Lucas Pintos <lucaspintos909@gmail.com>
2022-01-05 17:15:12 -0300

    feat: Usando hook para obtener gifs y pasandolos a ListOfGifs

--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ListOfGifs from "../../components/ListOfGifs";
+import { useGifs } from "../../hooks/useGifs";
 
-export default function SearchResults({params}) {
-  return(<ListOfGifs props={params}/>)
+export default function SearchResults({ params }) {
+  const { keyword } = params;
+
+  const { gifs } = useGifs({ keyword });
+
+  return <ListOfGifs gifs={gifs} />;
 }
