1f16ef7af67d27f6eb15ccf80c2984d91863e1ec
Lucas Pintos <lucaspintos909@gmail.com>
2022-01-05 16:35:02 -0300

    feat(Home): Añadiendo buscador mejorado al home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { useLocation } from "wouter";
 import { Link } from "wouter";
 import "./Home.css";
 
@@ -14,8 +15,36 @@ export default function Home() {
     "Perros",
   ];
 
+  const [keyword, setKeyword] = useState("");
+
+  const [path, pushLocation] = useLocation();
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    pushLocation(`/search/${keyword}`);
+  };
+
+  const handleChange = (evt) => {
+    setKeyword(evt.target.value);
+  };
+
   return (
     <div className="home">
+      <form className="home__form" onSubmit={handleSubmit}>
+        <label htmlFor="searchInput" className="form__label">
+          Buscar gif
+        </label>
+        <input
+          id="searchInput"
+          type="text"
+          className="form__input"
+          autoComplete="off"
+          placeholder="Ingrese la búsqueda"
+          value={keyword}
+          onChange={handleChange}
+        />
+      </form>
+
       <h2 className="home__title">Los gifs más populares</h2>
       <ul className="popular__gifs-list">
         {popular_gifs.map((gif) => (
