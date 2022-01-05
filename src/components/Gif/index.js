22bbe2c1dc0056c9971ecc4077a1e2677d3a5b09
Lucas Pintos <lucaspintos909@gmail.com>
2022-01-05 16:31:03 -0300

    feat: Usando Link para redirigir al detalle del gif

--- src/components/Gif/index.js
+++ src/components/Gif/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./Gif.css";
 
+import { Link } from "wouter";
+
 export default function Gif({ id, title, url }) {
   return (
-    <a href={`#${id}`} className="gif">
+    <Link to={`/gif/${id}`} className="gif">
       <img className="gif__img" src={url} alt={title} />
       {/* <p className="gif__title">{title}</p>
       <small className="gif__id">{id}</small> */}
-    </a>
+    </Link>
   );
 }
