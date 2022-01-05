44c97f82dca1a04ae9a023c2bf42ddcda1c0d621
Lucas Pintos <lucaspintos909@gmail.com>
2022-01-05 20:58:54 -0300

    feat: Guardando ultimos gif buscados en localStorage y usandolo

--- src/hooks/useGifs.js
+++ src/hooks/useGifs.js
@@ -1,12 +1,15 @@
 import { useState, useEffect } from "react";
 import getGifs from "../services/getGifs";
 
-export function useGifs({ keyword }) {
+export function useGifs({ keyword } = { keyword: null }) {
   const [gifs, setGifs] = useState([]);
 
+  const keywordToUse = keyword || localStorage.getItem('lastKeyword') || 'random';
+
   useEffect(() => {
-    getGifs({ keyword }).then((newGifs) => {
+    getGifs({ keyword: keywordToUse }).then((newGifs) => {
       setGifs(newGifs);
+      localStorage.setItem('lastKeyword', keyword);
     });
   }, [keyword]);
 
