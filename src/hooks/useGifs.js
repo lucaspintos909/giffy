f1e985a9df20794861c095bbea1a7549e63a0530
Lucas Pintos <lucaspintos909@gmail.com>
2022-01-05 17:10:46 -0300

    feat: Custom hook para devolver gifs segun la keyword

---
+++ src/hooks/useGifs.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import getGifs from "../services/getGifs";
+
+export function useGifs({ keyword }) {
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(() => {
+    getGifs({ keyword }).then((newGifs) => {
+      setGifs(newGifs);
+    });
+  }, [keyword]);
+
+  return gifs;
+}
