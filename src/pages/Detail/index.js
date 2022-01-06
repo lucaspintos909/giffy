cdacfe6a6e744f41cf7113761d7f76ea10bfa7e6
Lucas Pintos <lucaspintos909@gmail.com>
2022-01-06 19:39:04 -0300

    refactor: Obteniendo los gifs del hook en vez del context directo

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,15 +1,14 @@
-import React, { useContext } from "react";
-import GifsContext from "../../context/GifsContext";
+import React from "react";
 import Gif from "../../components/Gif";
+import { useGlobalGifs } from "../../hooks/useGlobalGifs";
 
 import "./Detail.css";
 
 export default function Detail({ params }) {
-  const { gifs } = useContext(GifsContext);
+  const gifs = useGlobalGifs();
 
   const gif = gifs.find((singleGif) => singleGif.id === params.id);
 
-  console.log(gif);
   {/* <div>
     <p>{gif.title}</p>
     <img src={gif.url} alt="" />
