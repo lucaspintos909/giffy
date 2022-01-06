a8edd0ee496e36632342d93fbdea5639810799a6
Lucas Pintos <lucaspintos909@gmail.com>
2022-01-05 21:48:41 -0300

    feat: Obteniendo gifs del contexto y buscando por id

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import GifsContext from "../../context/GifsContext";
+import Gif from "../../components/Gif";
+
 import "./Detail.css";
 
 export default function Detail({ params }) {
-  const { id } = params;
-  return <div>Gif: {id}</div>;
+  const { gifs } = useContext(GifsContext);
+
+  const gif = gifs.find((singleGif) => singleGif.id === params.id);
+
+  console.log(gif);
+  {/* <div>
+    <p>{gif.title}</p>
+    <img src={gif.url} alt="" />
+  </div> */}
+  return <Gif {...gif} />;
 }
