7878fe5ab76e9bccce64ddaf2ab3c4be30a07ba0
Lucas Pintos <lucaspintos909@gmail.com>
2022-01-15 19:28:27 -0300

    feat: Button to get more gifs in search results

--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -2,15 +2,25 @@ import React from "react";
 import ListOfGifs from "components/ListOfGifs";
 import { useGifs } from "hooks/useGifs";
 
+import "./index.css";
+
 export default function SearchResults({ params }) {
   const { keyword } = params;
 
-  const { gifs } = useGifs({ keyword });
+  const { gifs, setPage } = useGifs({ keyword });
+
+  const handleNextPage = () => setPage((prevPage) => prevPage + 1);
 
   return (
     <>
       <h2>Busqueda: {decodeURI(keyword)}</h2>
       <ListOfGifs gifs={gifs} />
+
+      <div className="navigation">
+        <button onClick={handleNextPage} className="navigation__btn btn">
+          Mostrar más
+        </button>
+      </div>
     </>
   );
 }
