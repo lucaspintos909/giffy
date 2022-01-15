fe615342afdac739636b1088d11227e2dd43b775
Lucas Pintos <lucaspintos909@gmail.com>
2022-01-14 22:39:59 -0300

    feat(TrendingSearches.js): Getting trending searches

---
+++ src/components/TrendingSearches/index.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "wouter";
+
+import getTrendingTerms from "../../services/getTrendingTermsService";
+
+import "./index.css";
+
+export default function TrendingSearches() {
+  const [trends, setTrends] = useState([]);
+
+  useEffect(() => {
+    getTrendingTerms().then((newTrends) => setTrends(newTrends));
+  }, []);
+
+  return (
+    <div>
+      <h2 className="home__title">Los gifs más populares</h2>
+      <ul className="popular__gifs-list">
+        {trends.map((gif) => (
+          <li key={gif}>
+            <Link className="popular__gifs-link" to={`/search/${gif}`}>
+              Gifs de {gif}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
