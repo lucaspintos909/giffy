a47d90d93ad9b040ede64a0af9f6a1261702206d
Lucas Pintos <lucaspintos909@gmail.com>
2022-01-15 18:56:17 -0300

    feat: Added lazy loading to the list of trending searches

--- src/components/TrendingSearches/index.js
+++ src/components/TrendingSearches/index.js
@@ -1,29 +1,19 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "wouter";
-
-import getTrendingTerms from "services/getTrendingTermsService";
+import React, { Suspense } from "react";
+import { useNearElement } from "hooks/useNearElement";
+import Loader from "./TrendingSearchesLoader";
 
 import "./index.css";
 
-export default function TrendingSearches() {
-  const [trends, setTrends] = useState([]);
+const TrendingSearches = React.lazy(() => import("./TrendingSearches"));
 
-  useEffect(() => {
-    getTrendingTerms().then((newTrends) => setTrends(newTrends));
-  }, []);
+export default function LazyTrendingSearches() {
+  const { isNearElement, elementRef } = useNearElement();
 
   return (
-    <div>
-      <h2 className="home__title">Los gifs más populares</h2>
-      <ul className="popular__gifs-list">
-        {trends.map((gif) => (
-          <li key={gif}>
-            <Link className="popular__gifs-link" to={`/search/${gif}`}>
-              Gifs de {gif}
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <div ref={elementRef}>
+      <Suspense fallback={<Loader/>}>
+        {isNearElement ? <TrendingSearches /> : <Loader/>}
+      </Suspense>
     </div>
   );
 }
