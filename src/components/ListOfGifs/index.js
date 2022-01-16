f8aec195f2b0f435b0853715c3584c49e3089719
Lucas Pintos <lucaspintos909@gmail.com>
2022-01-15 21:11:50 -0300

    fix: Rename the gifs__list class name

--- src/components/ListOfGifs/index.js
+++ src/components/ListOfGifs/index.js
@@ -5,7 +5,7 @@ import "./index.css";
 
 export default function ListOfGifs({ gifs }) {
   return (
-    <div className="list__gifs">
+    <div className="gifs__list">
       {gifs.map(({ id, title, url }) => (
         <Gif id={id} title={title} url={url} key={id} />
       ))}
