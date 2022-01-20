daf7cd53f763d9500de5680bd1609de69d3a544f
Lucas Pintos <lucaspintos909@gmail.com>
2022-01-20 15:52:03 -0300

    feat(Detail): Scroll up when component is created

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Gif from "components/Gif";
 import { useGlobalGifs } from "hooks/useGlobalGifs";
 
@@ -8,6 +8,10 @@ export default function Detail({ params }) {
   const gifs = useGlobalGifs();
 
   const gif = gifs.find((singleGif) => singleGif.id === params.id);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  });
 
   return (
     <div className="detail">
