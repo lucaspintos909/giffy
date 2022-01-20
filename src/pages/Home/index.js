cece0a0449c3a30436c4cda621b5056d8163ec87
Lucas Pintos <lucaspintos909@gmail.com>
2022-01-20 15:43:57 -0300

    feat(Home): Add Searcher component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useGifs } from "hooks/useGifs";
 import ListOfGifs from "components/ListOfGifs";
+import Searcher from "components/Searcher";
 import TrendingSearches from "components/TrendingSearches";
 
 import "./index.css";
@@ -10,6 +11,8 @@ export default function Home() {
 
   return (
     <div className="home">
+      <Searcher />
+
       <h2 className="home__title">Última búsqueda: {decodeURI(lastKeyword)}</h2>
       <ListOfGifs gifs={gifs} />
 
