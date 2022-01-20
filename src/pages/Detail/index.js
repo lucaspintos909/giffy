21e6dcf7be81204936c41b1b2a0eb8c3fa11891f
Lucas Pintos <lucaspintos909@gmail.com>
2022-01-20 16:38:18 -0300

    feat(Detail): Getting gif from id

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,24 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Gif from "components/Gif";
 import { useGlobalGifs } from "hooks/useGlobalGifs";
+
+import { getGif } from "services/getGifs";
 
 import "./index.css";
 
 export default function Detail({ params }) {
   const gifs = useGlobalGifs();
 
-  const gif = gifs.find((singleGif) => singleGif.id === params.id);
+  const [gif, setGif] = useState(
+    gifs.find((singleGif) => singleGif.id === params.id)
+  );
 
   useEffect(() => {
     window.scrollTo(0, 0);
+
+    if (!gif) {
+      getGif({ id: params.id }).then((data) => {
+        setGif(data);
+      });
+    }
   });
 
   return (
     <div className="detail">
-      <div className="detail__header">
-        <h2 className="detail__title">{gif.title}</h2>
-      </div>
-      <Gif {...gif} />
+      {gif ? (
+        <>
+          <div className="detail__header">
+            <h2 className="detail__title">{gif.title}</h2>
+          </div>
+          <Gif {...gif} />{" "}
+        </>
+      ) : (
+        <div>Cargando gif...</div>
+      )}
     </div>
   );
 }
