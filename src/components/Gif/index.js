f0d157c7873a238c693d7d92111b2baf53e8bc37
Lucas Pintos <lucaspintos909@gmail.com>
2022-01-21 10:56:55 -0300

    feat: Adding lazy loading to images

--- src/components/Gif/index.js
+++ src/components/Gif/index.js
@@ -7,7 +7,7 @@ export default function Gif({ id, title, url }) {
   return (
     <div className="gif">
       <Link to={`/gif/${id}`} className="gif__link">
-        <img className="gif__img" src={url} alt={title} />
+        <img loading="lazy" className="gif__img" src={url} alt={title} />
       </Link>
     </div>
   );
